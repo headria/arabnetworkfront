8ecbd2c1d69bdfe3010b8f28e04138597fd4a26d
abas69 <lostangel619@gmail.com>
2022-05-20 16:55:24 +0430

    + about page

---
+++ src/components/Pages/About/OurVision.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import mobileScreenShot from '../../../assets/images/vision-app-screenshot.png'
+
+const OurVision = ({ t }) => {
+    return (
+        <>
+            <div className="ourvision-section pt-50 pb-100">
+                <div className="container">
+                    <div className="section-title text-center">
+                        <h3 className='title'>{t("our_vision.title")}</h3>
+                        <p>{t("our_vision.desc")}</p>
+                    </div>
+                    <div className="section-inner">
+                        <div className="row align-items-center">
+                            <div className="col-lg-7">
+                                <div className="text-content">
+                                    <div className="rotate-text">
+                                        <h1>{t("our_vision.built.title")}</h1>
+                                        <h2>{t("our_vision.built.desc")}</h2>
+                                    </div>
+                                    <div className="information">
+                                        <h6>{t("our_vision.information.title")}</h6>
+                                        <ul>
+                                            <li>{t("our_vision.information.ul.li_1")}</li>
+                                            <li>{t("our_vision.information.ul.li_2")}</li>
+                                            <li>{t("our_vision.information.ul.li_3")}</li>
+                                            <li>{t("our_vision.information.ul.li_4")}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-5">
+                                <div className="image-content">
+                                    <div className="img-inner">
+                                        <img src={mobileScreenShot} alt="img-name" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default OurVision
--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -5,6 +5,11 @@ import Footer from '../../Layouts/Footer'
 
 import { t } from "i18next";
 import { ThemeContext, Themes } from '../../Layouts/Themes/themeContext'
+import HeroAbout from './HeroAbout';
+import WhatisArabnetwork from './WhatisArabnetwork';
+import OurVision from './OurVision';
+import OurValues from './OurValues';
+import Teams from './Teams';
 
 const About = () => {
     const [drawer, drawerAction] = useState(false);
@@ -25,10 +30,13 @@ const About = () => {
                 themecontext={ThemeContext}
                 themes={Themes}
             />
-            {
-                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, corporis recusandae doloremque sed repudiandae possimus placeat ipsam necessitatibus nisi eaque voluptates unde ut odio omnis adipisci error accusamus, iure tenetur magnam odit delectus aperiam nulla est reiciendis? Autem sequi in, quas dolorum ea laborum voluptas enim libero expedita dolore reiciendis porro itaque, provident distinctio quidem iste iusto corrupti necessitatibus esse nulla numquam dolores, accusantium iure. Neque, doloribus laudantium illo tenetur commodi sint maiores beatae! Non nihil minima fuga debitis repellendus quibusdam in saepe dolorum unde nisi. Ipsam quae animi sunt reprehenderit consequuntur perspiciatis ratione magni, deserunt, sapiente vitae eligendi accusantium odio eaque saepe quo nam sequi amet distinctio dolores ab. Expedita, illo dolorem incidunt consequuntur molestiae, ex veniam eaque voluptas sapiente ipsum repellat illum, voluptatum mollitia! Illo odio ducimus similique iste excepturi possimus molestiae nemo sed provident consequatur reprehenderit facere eos vero ullam rem dolor voluptatibus aliquam, necessitatibus, quis cum itaque dolores natus est mollitia! Laborum laboriosam nesciunt sint porro corporis. Ipsum ducimus libero, odio recusandae quaerat quidem tenetur deserunt dolor alias ea cum dignissimos maxime consectetur natus laudantium assumenda provident temporibus consequuntur quo velit, dolores corporis vero. Quas asperiores facilis ullam, officiis quibusdam perspiciatis repellendus beatae et. Architecto, cumque?"
-            }
-            <Footer />
+            <HeroAbout />
+            <WhatisArabnetwork t={t} />
+            <OurVision t={t} />
+            <OurValues t={t} />
+            <Teams t={t} />
+
+            <Footer darkMode={darkMode} />
         </>
     )
 }
