0984147d582bbd53803ffc2efa868e497c08f324
headria <headria@gmail.com>
2022-06-18 16:25:53 +0300

    update partnership section

--- src/components/Pages/Home/Partnership.js
+++ src/components/Pages/Home/Partnership.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import pertner_arabnetwork_light from "../../../assets/images/partnership/partnership-arabnetwork-dark.png";
 import pertner_getblock_light from "../../../assets/images/partnership/partnership-getblock.png";
+import polyogn_light from "../../../assets/images/partnership/polygon-light.png";
+import polyogn_dark from "../../../assets/images/partnership/polygon-dark.png";
 import pertner_pinksale_light from "../../../assets/images/partnership/partnership-pinksale-dark.png";
 import pertner_pinksale_dark from "../../../assets/images/partnership/partnership-pinksale-light.png";
 
@@ -36,19 +38,19 @@ const Partnership = ({ t, lang, darkMode }) => {
                                     </div>
                                 </div>
                             </div> */}
-              {/* <div className="col-lg-4 col-md-12">
+              <div className="col-lg-4 col-md-12">
                 <div className="card-partnership">
                   <div className="card-inner">
                     <div className="img-content">
                       {darkMode ? (
-                        <img src={pertner_pinksale_light} alt="img-name" />
+                        <img src={polyogn_dark} alt="img-name" />
                       ) : (
-                        <img src={pertner_pinksale_dark} alt="img-name" />
+                        <img src={polyogn_light} alt="img-name" />
                       )}
                     </div>
                   </div>
                 </div>
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
