01842e60167603335dc82dbd068fd2eda1cfe6ff
abas69 <lostangel619@gmail.com>
2022-06-29 13:15:25 +0430

    fix ui partnership

--- src/components/Pages/Home/Partnership.js
+++ src/components/Pages/Home/Partnership.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper";
+import 'swiper/css'
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+
 import pertner_arabnetwork_light from "../../../assets/images/partnership/partnership-arabnetwork-dark.png";
 import pertner_getblock_light from "../../../assets/images/partnership/partnership-getblock.png";
-import polyogn_light from "../../../assets/images/partnership/polygon-light.png";
-import polyogn_dark from "../../../assets/images/partnership/polygon-dark.png";
+import polyogn_light from "../../../assets/images/partnership/partnership-polygon-light.png";
+import polyogn_dark from "../../../assets/images/partnership/partnership-polygon-dark.png";
 import pertner_pinksale_light from "../../../assets/images/partnership/partnership-pinksale-dark.png";
 import pertner_pinksale_dark from "../../../assets/images/partnership/partnership-pinksale-light.png";
+import pertner_ankr from "../../../assets/images/partnership/partnership-ankr.png";
 
 const Partnership = ({ t, lang, darkMode }) => {
   return (
@@ -19,8 +26,37 @@ const Partnership = ({ t, lang, darkMode }) => {
             <p>{t("partnership.desc")}</p>
           </div>
           <div className="section-inner">
-            <div className="row">
-              <div className="col-lg-4 col-md-12">
+
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              navigation={true}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              modules={[Navigation, Autoplay]}
+              className="swiper-teams"
+              breakpoints={{
+                450: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 15,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                992: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+              }}
+            >
+              <SwiperSlide>
                 <div className="card-partnership">
                   <div className="card-inner">
                     <div className="img-content">
@@ -28,17 +64,8 @@ const Partnership = ({ t, lang, darkMode }) => {
                     </div>
                   </div>
                 </div>
-              </div>
-              {/* <div className="col-lg-4 col-md-12">
-                                <div className="card-partnership">
-                                    <div className="card-inner">
-                                        <div className="img-content">
-                                            <img src={pertner_arabnetwork_light} alt="img-name" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> */}
-              <div className="col-lg-4 col-md-12">
+              </SwiperSlide>
+              <SwiperSlide>
                 <div className="card-partnership">
                   <div className="card-inner">
                     <div className="img-content">
@@ -50,8 +77,27 @@ const Partnership = ({ t, lang, darkMode }) => {
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card-partnership">
+                  <div className="card-inner">
+                    <div className="img-content">
+                      <img src={pertner_ankr} alt="img-name" />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card-partnership">
+                  <div className="card-inner">
+                    <div className="img-content">
+                      <img src={pertner_ankr} alt="img-name" />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+
           </div>
         </div>
       </div>
